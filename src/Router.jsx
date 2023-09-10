3ac2c94cd7e481ecefb2724c518d9489c460b60e
Andresr35 <andresjruiz@cpp.edu>
2023-09-09 21:13:04 -0700

    setup css modules and router

---
+++ src/Router.jsx
@@ -0,0 +1,9 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const Router = () => {
+  const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
