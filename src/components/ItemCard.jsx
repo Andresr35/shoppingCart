9a261dd02a4cdb387ec494cc2f233ca529eca0da
Andresr35 <andresjruiz@cpp.edu>
2023-09-15 13:31:38 -0700

    finish fetching from fakestore api and start css for shopping page

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,36 +1,23 @@
-import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import styles from "../assets/ItemCard.module.css";
+import Star from "./Star";
 
-const ItemCard = () => {
-  const [product, setProduct] = useState({
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    rating: {
-      rate: 3.9,
-      count: 120,
-    },
-  });
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products/1")
-      .then((res) => res.json())
-      .then((json) => setProduct(json));
-  }, [product]);
-
+const ItemCard = ({ product }) => {
   return (
-    <div>
+    <div className={styles.itemCard}>
       <h4>{product.title}</h4>
       <img src={product.image} alt="productImage" />
-      <p>{product.price}</p>
+      <p>${product.price}</p>
       <p>{product.description}</p>
       <p>
-        {product.rating.rate} with {product.rating.count} ratings!
+        {product.rating.rate} <Star /> with {product.rating.count} ratings!
       </p>
     </div>
   );
+};
+
+ItemCard.propTypes = {
+  product: PropTypes.object.isRequired,
 };
 
 export default ItemCard;
--- src/components/ProductCategoryList.jsx
+++ src/components/ProductCategoryList.jsx
@@ -1,38 +1,65 @@
 import { useEffect, useState } from "react";
 import styles from "../assets/ProductCategoryList.module.css";
+import ItemCard from "./ItemCard";
 
 const ProductCategoryList = () => {
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState({});
+  const [products, setProducts] = useState(null);
+
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/categories")
-      .then((res) => res.json())
-      .then((json) => setCategories(json))
-      .then(() => {
-        categories.forEach((category) => {
-          fetch(`https://fakestoreapi.com/products/category/jewelery}`)
-            .then((res) => res.json())
-            .then((json) => {
-              const newProducts = { ...products, [category]: json };
-              setProducts(newProducts);
-            });
+    const fetchCategories = async () => {
+      try {
+        const res = await fetch(
+          "https://fakestoreapi.com/products/categories",
+          {
+            mode: "cors",
+          }
+        );
+        const json = await res.json();
+        return json;
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
+    };
+    const fetchProducts = async () => {
+      try {
+        const arr = await fetchCategories();
+        let newProducts;
+        await arr.forEach(async (category) => {
+          const res = await fetch(
+            `https://fakestoreapi.com/products/category/${category}`,
+            {
+              mode: "cors",
+            }
+          );
+          const arr = await res.json();
+          if (newProducts === null) {
+            newProducts = { [category]: arr };
+          } else {
+            newProducts = { ...newProducts, [category]: arr };
+          }
+          setProducts(newProducts);
         });
-      })
-      .catch((error) => console.error("error fetching", error));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProducts();
   }, []);
 
   return (
-    <div className={styles.products}>
-      stuff goes under here
-      {Object.keys(products).map((category, index) => (
-        <div key={index} className={styles.category}>
-          <div>{category}</div>
-
-          {products[category].map((product, index) => (
-            <div key={index}>{product.title}</div>
-          ))}
-        </div>
-      ))}
+    <div className={styles.categories}>
+      {products &&
+        Object.keys(products).map((key) => (
+          <div key={key}>
+            <h4>{key}</h4>
+            <div className={styles.category} key={key}>
+              {products[key].map((product, index) => (
+                <ItemCard key={index} product={product} />
+              ))}
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
